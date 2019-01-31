7e57783e2a314c0190ee0cf2057d3d22fb7cda0f
Brian De Maio <bdemaio1@gmail.com>
2019-01-30 19:52:33 -0500

    git cache cleared

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const App = () => {
+  return (<div>
+      <BrowserRouter>
+        <div>
+            <Route path="/" component={}/>
+        </div>
+      </BrowserRouter>  
+  </div>);
+};
+
+export default App;
