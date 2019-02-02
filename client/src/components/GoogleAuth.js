bbc0f0679a4e27e76f8ccc15392f9f9562ebff60
Brian De Maio <bdemaio1@gmail.com>
2019-02-02 17:59:44 -0500

    Added Google icon and stylign to Sign In and Sign Out button option

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -9,15 +9,16 @@ class GoogleAuth extends Component {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button className="ui red google button" onClick={this.onSignOut}>
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui red google button">
-          <i className="google icon">Sign In With Google</i>
+        <button className="ui red google button" onClick={this.onSignIn}>
+          <i className="google icon" />
+          Sign In With Google
         </button>
       );
     }
@@ -25,6 +26,14 @@ class GoogleAuth extends Component {
 
   onAuthChange = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
   };
 
   componentDidMount() {
