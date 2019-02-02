64cba4c04d0760cd3770cd95884989721b0c242b
Brian De Maio <bdemaio1@gmail.com>
2019-02-02 16:53:47 -0500

    Displaying current auth signed in status, for Google Oauth div in header

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn == null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>You are signed in</div>;
+    } else {
+      return <div>You are not signed in</div>;
+    }
+  }
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId: "",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientId: "",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
 
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
