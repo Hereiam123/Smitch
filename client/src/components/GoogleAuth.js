8fa432686db5e108e43b2439a19fdcb9ff3ad4e1
hereiam123 <bdemaio1@gmail.com>
2019-02-03 13:53:10 -0500

    Adding Redux form library

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -16,6 +16,7 @@ class GoogleAuth extends Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.onAuthChange(this.auth.isSignedIn.get());
+          //Listen to Signed In value
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
