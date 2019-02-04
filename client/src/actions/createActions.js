7e5f0a0e96b21c918e6ca9526199b2e70e9eea17
hereiam123 <bdemaio1@gmail.com>
2019-02-03 19:56:13 -0500

    Adding logged in user id to creating stream form submission for persistance

--- client/src/actions/createActions.js
+++ client/src/actions/createActions.js
@@ -2,7 +2,7 @@ import streams from "../apis/streams";
 import { CREATE_STREAM } from "./types";
 
 export const createStream = formValues => async (dispatch, getState) => {
-  const userId = getState.auth;
+  const { userId } = getState().auth;
   const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
