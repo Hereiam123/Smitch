46851e850fe83b298460de5bac8304e9f8bd16dd
hereiam123 <bdemaio1@gmail.com>
2019-02-03 20:05:53 -0500

    Adding Edit and Delete buttons to StreamList componenet for streams created by logged in user

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchStreams } from "../../actions/fetchActions";
 
 class StreamList extends Component {
@@ -7,10 +8,26 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
+  renderCurrUserOptions(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <Link to="/">
+            <button className="ui button primary"> Edit</button>
+          </Link>
+          <Link to="/">
+            <button className="ui button negative"> Delete</button>
+          </Link>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderCurrUserOptions(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">{stream.title}</div>
           <div className="description">{stream.description}</div>
@@ -30,7 +47,10 @@ class StreamList extends Component {
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 };
 
 export default connect(
