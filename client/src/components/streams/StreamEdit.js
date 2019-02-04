e085232e856de547ae91724d4bb8cd4e1d44aa95
hereiam123 <bdemaio1@gmail.com>
2019-02-03 21:38:34 -0500

    Using StreamForm within StreamEdit component and passing in initial values to be displayed

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
+import StreamForm from "./StreamForm";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions/fetchActions";
+import { editStream } from "../../actions/editActions";
 
 class StreamEdit extends Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     } else {
-      return <div>{this.props.stream.title}</div>;
+      return (
+        <div>
+          <h3>Edit a Stream</h3>
+          <StreamForm
+            onSubmit={this.onSubmit}
+            initialValues={this.props.stream}
+          />
+        </div>
+      );
     }
   }
 }
@@ -22,5 +36,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, editStream }
 )(StreamEdit);
