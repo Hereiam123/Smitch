c4ee74db663eaf8135589afcc1672cf76fc66328
hereiam123 <bdemaio1@gmail.com>
2019-02-03 20:34:20 -0500

    Custom created history used to send user back to home page on stream creation, within createActions file

--- client/src/actions/createActions.js
+++ client/src/actions/createActions.js
@@ -1,9 +1,11 @@
 import streams from "../apis/streams";
 import { CREATE_STREAM } from "./types";
+import history from "../history";
 
 export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
-  //Send user back to home page
+  //Send user back to home page if successful stream creation
+  history.push("/");
 };
