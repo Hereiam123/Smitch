c2b2d372ff2165148c573893d23b8f6cabb85508
hereiam123 <bdemaio1@gmail.com>
2019-02-04 22:11:03 -0500

    Can now delete a stream through the StreamDelete component, and re-route user to main page

--- client/src/actions/deleteActions.js
+++ client/src/actions/deleteActions.js
@@ -1,7 +1,9 @@
 import streams from "../apis/streams";
 import { DELETE_STREAM } from "./types";
+import history from "../history";
 
 export const deleteStream = streamId => async dispatch => {
   await streams.delete(`/streams/${streamId}`);
   dispatch({ type: DELETE_STREAM, payload: streamId });
+  history.push("/");
 };
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import Modal from "../Modal";
 import history from "../../history";
 import { fetchStream } from "../../actions/fetchActions";
+import { deleteStream } from "../../actions/deleteActions";
 
 class StreamDelete extends Component {
   componentDidMount() {
@@ -10,10 +12,18 @@ class StreamDelete extends Component {
   }
 
   renderActions() {
+    const { id } = this.props.match.params;
     return (
       <React.Fragment>
-        <button className="ui button negative">Delete</button>
-        <button className="ui button">Cancel</button>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
       </React.Fragment>
     );
   }
@@ -46,5 +56,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, deleteStream }
 )(StreamDelete);
