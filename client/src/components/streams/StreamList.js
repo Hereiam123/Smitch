620645282ed7f6052cb2abb975d20bff8889f01b
hereiam123 <bdemaio1@gmail.com>
2019-02-04 22:25:36 -0500

    Adding content to show for StreamShow component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -33,7 +33,7 @@ class StreamList extends Component {
           {this.renderCurrUserOptions(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            <Link to={`streams/show/${stream.id}`} className="header">
+            <Link to={`streams/${stream.id}`} className="header">
               {stream.title}{" "}
             </Link>
             <div className="description">{stream.description}</div>
